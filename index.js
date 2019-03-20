6c9f425795a1233bb65783ff5475f46af8da058b
Matt Williams <matt.seongjun@gmail.com>
2019-03-20 15:23:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ function nestedTarget() {
 function increaseRankBy(n) {
   
 }
+
+function nestedTarget() {
+  let current = document.getElementById('nested');
+  let next = [];
+  while (current || current === 0) {
+    if (current.querySelector('div.target')) {
+      return current.querySelector('div.target');
+    }
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i]);
+      }
+    }
+    current = next.shift();
+  }
+  return null;
+}
