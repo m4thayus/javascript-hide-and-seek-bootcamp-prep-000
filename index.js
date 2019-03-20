461ed8e75738899e001f46c2f30cecf92e12799d
Matt Williams <matt.seongjun@gmail.com>
2019-03-20 15:39:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  let current = document.getElementById('nested');
+  let next = [];
+  while (current || current === 0) {
+    if (current.querySelector('div.target')) {
+      return current.querySelector('div.target');
+    }
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i]);
+      }
+    }
+    current = next.shift();
+  }
+  return null;
+}
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  let current = document.getElementById('grand-node');
+  let next = [];
+  while (current || current === 0) {
+    var c = 0;
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i]);
+        c++;
+      }
+    }
+    current = next.shift();
+  }
+  return c;
+}
+
+deepestChild();
